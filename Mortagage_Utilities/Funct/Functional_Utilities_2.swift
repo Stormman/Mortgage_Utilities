34af06a89f972bd5e6163fc9934ce5b25cc7317e
Antonio Muñoz Sánchez <antoniomunoz@iMac-de-Antonio.local>
2017-11-01 09:46:55 +0100

    cambios

---
+++ Mortagage_Utilities/Funct/Functional_Utilities_2.swift
@@ -0,0 +1,9 @@
+//
+//  Functional_Utilities_2.swift
+//  Mortagage_Utilities
+//
+//  Created by Antonio Muñoz on 1/11/17.
+//  Copyright © 2017 Antonio Muñoz. All rights reserved.
+//
+
+import Foundation
