d6186246ba6194cc953cce81d6fd7a2babbd3582
Antonio Muñoz Sánchez <antoniomunoz@iMac-de-Antonio.local>
2017-11-24 09:41:49 +0100

    add mortagage2

---
+++ Mortagage_Utilities/Funct/Mix_Mortgage2.swift
@@ -0,0 +1,30 @@
+//
+//  Mix_Mortgage2.swift
+//  Mortagage_Utilities
+//
+//  Created by Antonio Muñoz on 23/11/17.
+//  Copyright © 2017 Antonio Muñoz. All rights reserved.
+//
+
+import Foundation
+
+
+struct PortFolio {
+   
+
+    
+    
+    
+    
+    
+    
+    
+}
+
+struct prestamoHipotecario {
+    
+    
+    
+}
+
+
