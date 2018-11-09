04e261f62932f46991c31cd7b1aca91a8d6a9403
Antonio Muñoz <srdolar1976@gmail.com>
2018-11-09 12:58:15 +0100

    ultimo

--- Mortagage_UtilitiesTests/cashflowTotal_Tests.swift
+++ Mortagage_UtilitiesTests/cashflowTotal_Tests.swift
@@ -12,20 +12,79 @@ import XCTest
 
 class cashflowTotal_Tests: XCTestCase {
     
-    func test_simulHipot () {
+    func test_simulHipot_and_cfds () {
         
         
-        let smih = simulHipot(presta_: 100000, años_: 25, eurib_: 1, diferen_: 0.5)
+        let smih = simulHipot(presta_: 100000, años_: 20, eurib_: 1, diferen_: 0.5)
         
         let f = smih.nextPeriod(numeroPeriodos: 1)
         
-        let hipCash = HipotecaCashFlowing(presta_: 100000, años_: 25, eurib_: 1, diferen_: 1, indiceSuscrip: "euribor", diasDeCo: 18, contador: 0, fechaInicioCnt: tDate_(1, 1, 2018)!, tramoF: (nil,nil), tipoFix: 3)
+        let hipCash = HipotecaCashFlowing(presta_: 100000, años_: 20, eurib_: 1, diferen_: 1, indiceSuscrip: "euribor", diasDeCo: 18, contador: 0, fechaInicioCnt: tDate_(1, 1, 2018)!, tramoF: (nil,nil), tipoFix: 3)
+        
+        
+        let cfd = CFDTot(priceBu: 100, priceV: 101, nContr: 10, mult: 10, contador: 0, indiceSus: "euribor", garantiasPCon: 700)
+        
+        
+        let cash = CashFlowSimul(fechaInic: tDate_(1, 1, 2018)!, fechaFin: tDate_(31, 12, 2038)!, saldoInicial: 120000)
+        
+        cash.addGtempo(ne: hipCash, isMain: true )
+        cash.addGtempo(ne: cfd, isMain: false )
+        cash.addGlobalIndic(nameInd: "euribor", valuer: 105)
+        
+        
+        let arr = Array(1...100)
+        
+        let ot = arr <==> {(_)-> Double  in
+            
+          cash.nextPeriod()
+            
+            return cash.saldoTotal
+            
+            
+        }
+        
+        
+        let ff = 900000
         
         
         
         
         
         
+        
+    }
+
+    
+    func test_cfdsSimul() {
+        
+        
+        let cfd = CFDTot(priceBu: 100, priceV: 101, nContr: 10, mult: 10, contador: 0, indiceSus: "euribor", garantiasPCon: 700)
+        
+        
+        let cash = CashFlowSimul(fechaInic: tDate_(1, 1, 2018)!, fechaFin: tDate_(31, 12, 2038)!, saldoInicial: 120000)
+        
+        
+        cash.addGtempo(ne: cfd, isMain: false )
+        cash.addGlobalIndic(nameInd: "euribor", valuer: 105)
+        
+        
+        let ot = Array(1...100) <==> {(_)-> Double  in
+            
+            cash.nextPeriod()
+            
+            return cash.saldoTotal
+            
+            
+        }
+        
+        cfd.addContratos(nCont: -1)
+        
+        cash.nextPeriod()
+        
+        let sal = cash.saldoTotal
+        
+        
+        let vpp = 900000
         
         
         
